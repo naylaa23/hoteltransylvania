85b679411e1084ea9a26f7b9826966150f488ce9
alfathrev <pranadaalfath@gmail.com>
2026-04-25 14:24:47 +0700

    memperbaiki main java

--- src/main/java/com/mycompany/transylvaniahotel/Main.java
+++ src/main/java/com/mycompany/transylvaniahotel/Main.java
@@ -1,6 +1,6 @@
 package com.mycompany.transylvaniahotel;
 
-// Import DAO dari Pertemuan 4
+
 import com.mycompany.transylvania.hotel.dao.BaseDAO;
 import com.mycompany.transylvania.hotel.dao.GenericDAO;
 
@@ -12,22 +12,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== Project Transylvania: Sistem Manajemen Hotel RFID ===");
         
-        // 1. Inisialisasi Universal Adapter (Generic DAO)
-        // Perhatikan bagaimana satu kelas GenericDAO bisa menangani dua tipe berbeda
+       
         BaseDAO<Tamu> tamuRepo = new GenericDAO<>(Tamu.class);
         BaseDAO<LogAbsensi> logRepo = new GenericDAO<>(LogAbsensi.class);
 
-        // 2. Simulasi Data Tamu (Data dari Pertemuan 3)
+       
         Tamu tamuBaru = new Tamu("RFID-998877", "BK-001", "Dracula", "Room 666");
         
-        // 3. Test Simpan Data Tamu
+       
         System.out.println("\n--- Menguji DAO Tamu ---");
         tamuRepo.save(tamuBaru);
 
-        // 4. Simulasi Log Absensi RFID
+       
         LogAbsensi logMasuk = new LogAbsensi("LOG-01", "RFID-998877", LocalDateTime.now(), "IN");
         
-        // 5. Test Simpan Data Log
+       
         System.out.println("\n--- Menguji DAO Log Absensi ---");
         logRepo.save(logMasuk);
         
